83653dabb0b128dc8268b28f968596fd5460f1c0
Nikita <myk.holitsyn@gmail.com>
2021-09-17 18:11:46 +0300

    all

---
+++ lesson8/task2/src/index.js
@@ -0,0 +1,13 @@
+import { initTodoListHandler } from "./list/todoList.js";
+import { renderTasks } from "./list/render.js";
+import { getTasksList } from "./list/tasksGateway.js";
+import "./index.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTasksList().then((taskslist) => renderTasks(taskslist));
+
+  initTodoListHandler();
+});
+
+// 1. Get data from server
+// 2. Save data to front-end storage
